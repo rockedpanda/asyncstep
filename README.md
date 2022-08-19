158bf18123dfb28f3b2f81e5f338fbe506e1bab3
RockedPanda <chenghuawei@gmail.com>
2022-08-19 10:40:34 +0800

    初始化版本,支持自动启动

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # asyncstep
-async steps for NodeJS tasks
+async steps for NodeJS tasks;
+
+支持暂停和继续的异步任务队列处理,支持设置同时并发数量(默认为3);
+
+```javascript
+/**
+ * 对一组数据由多个消费者进行异步处理,并可以获取全部处理后的结果数据
+ * @param {Array} list 原始数据数组
+ * @param {Function} func 单条数据的处理函数,要求返回Promise对象,入参为单条数据和数据序号
+ * @param {Number} startIndex 从几条数据开始启动,默认为0
+ * @param {Number} workers 最大并行数量
+ * @param {Boolean} autoStart 是否创建后立即启动任务
+ * @return AsyncStep实例, 该实例的promise属性是一个表示是否全部结束的Promise对象
+ */
+const tasks = new StepByStep(list, func, 0, 3, false);
+task.promise.then(log,log);  //可以通过tasks的promise属性监听结束消息
+task.run();    //启动
+task.pause();  //暂停, 进行中的worker不会立即停止,但暂停后不会有下一个worker启动
+task.run();    //启动, 如果是暂停状态则切换为正常状态
+```
+
+## install
+
+```sh
+npm install asyncstep
+```
+## usage
+
+```javascript
+const AsyncStep = require('asyncstep');
+const log = console.log.bind(console);
+//测试用数据
+const list = [1,2,3,4,1,2,3,4,111,111,1111,121]
+ 
+//单个数据的处理函数, 用随机数控制的计时器模拟异步
+const dealer = function(i){
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      console.log(i, new Date());
+      resolve(i+100);
+    },Math.random()*200>>0);
+  });
+}
+
+//创建后手动启动
+//var a = new StepByStep(list, dealer, 0, 3, true);
+//a.promise.then(log, log);
+//a.run();
+//a.pause();
+//a.run();
+
+//创建后立即启动
+new AsyncStep(list,dealer,0,3, true).promise.then(log,log);
+
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * 对一组数据由多个消费者进行异步处理,并可以获取全部处理后的结果数据
+ * @param {Array} list 原始数据数组
+ * @param {Function} func 单条数据的处理函数,要求返回Promise对象,入参为单条数据和数据序号
+ * @param {Number} startIndex 从几条数据开始启动,默认为0
+ * @param {Number} workers 最大并行数量
+ * @param {Boolean} autoStart 是否创建后立即启动任务
+ * @return AsyncStep实例, 该实例的promise属性是一个表示是否全部结束的Promise对象
+ */
+ function AsyncStep(list, func, startIndex=0, workers=3, autoStart=false){
+	this.list = list;    //待处理数据
+	this.func = func;    //单个任务执行处理函数
+	this.index = startIndex||0;   //当前索引
+	this.workers = Math.max(1, workers);   //最大并行数量,默认为3,最小为1
+	this.thread = 0;  //当前执行中任务数量
+  this.ans = [];    //所有结果缓存
+  let that = this;
+  this.promise = new Promise(function(resolve,reject){  //整体promise对象
+    that.resolve = resolve;
+    that.reject = reject;
+  });
+  if(autoStart){  //自动开始
+	  this.run();
+  }
+}
+AsyncStep.prototype.run = function(){
+	this.wait = false;
+	for(let i=0;i<this.workers;i++){
+		this.next();
+	}
+};
+AsyncStep.prototype.next = function(){
+	if(this.index >= this.list.length){
+    if(this.thread===0){ //已全部结束
+      this.resolve && this.resolve(this.ans);
+      return;
+    }
+		return;
+	}
+	if(this.thread >= this.workers){
+		//console.log('忙碌中,稍后重试');
+		return;
+	}
+	this.goOneStep();
+};
+AsyncStep.prototype.goOneStep = function(){
+	this.thread++;
+  let i = this.index;
+	let data = this.list[this.index];
+	this.index++;
+	return this.func(data).then((ans)=>{
+    this.ans[i] = ans;
+		this.thread--;
+		this.next();
+	},(err)=>{
+    console.log(err);
+    this.ans[i] = null;
+		this.thread--;
+		this.next();
+	});
+};
+AsyncStep.prototype.pause = function(){
+	this.wait = true;
+	this.thread = 0;
+};
+
+module.exports = AsyncStep;
